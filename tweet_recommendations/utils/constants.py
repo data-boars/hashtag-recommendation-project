cb3fd4e5ba40ba6e8f9af7f4b031b02b1a6d3b8c
Jakub Michalowski <jakub.michalowski@mirumee.com>
2018-12-15 17:05:04 +0100

    Unify embeddings package.
    Refactor fast_text module.
    Delete unused directories/files.

--- tweet_recommendations/utils/constants.py
+++ tweet_recommendations/utils/constants.py
@@ -2,3 +2,4 @@
 EXTERNAL_DATA_PATH = "/path/to/be/determined/{filename}"
 OUTPUT_DATA_PATH = "/path/to/be/determined/{filename}"
 WORD2VEC_MODEL_PATH = "/path/to/model"
+FASTTEXT_MODEL_PATH = "/path/to/model"
