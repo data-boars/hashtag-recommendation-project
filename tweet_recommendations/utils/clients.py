5094b6e2601824f9679a9ce53649f64ffc80ebc9
Jakub Michalowski <jakub.michalowski@mirumee.com>
2018-12-19 08:25:07 +0100

    Review comments

--- tweet_recommendations/utils/clients.py
+++ tweet_recommendations/utils/clients.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,7 +7,7 @@ WCRFT2_ENDPOINT = "http://ws.clarin-pl.eu/nlprest2/base/process/"
 WCRFT2_JSON_TEMPLATE = {"lpmn": "any2txt|wcrft2", "text": "", "user": "sample@mail.com"}
 
 
-def parse_wcrft_response(xml_response: str):
+def parse_wcrft_response(xml_response: str) -> List[str]:
     xml_soup = BeautifulSoup(xml_response, features="xml")
     all_tags = xml_soup.find_all("base")
     all_words = [tag.text for tag in all_tags]
@@ -13,8 +15,8 @@ def parse_wcrft_response(xml_response: str):
     return all_words
 
 
-def get_wcrft2_results_for_text(tweet_to_process):
-    tagged_tweet_words_as_list = []
+def get_wcrft2_results_for_text(tweet_to_process: str) -> List[str]:
+    lemmatized_tweet_words_as_list = []
     request_body = WCRFT2_JSON_TEMPLATE
     request_body["text"] = tweet_to_process
 
@@ -26,11 +28,11 @@ def get_wcrft2_results_for_text(tweet_to_process):
     if status_code == 200:
         r.encoding = "ISO-8859-1"
         xml_response = r.content
-        tagged_tweet_words_as_list = parse_wcrft_response(xml_response)
+        lemmatized_tweet_words_as_list = parse_wcrft_response(xml_response)
     else:
         print(
             "Something went wrong with clarin service - status code: {}".format(
                 status_code
             )
         )
-    return tagged_tweet_words_as_list
+    return lemmatized_tweet_words_as_list
