ae46c9d393e548cb5990693c98be6b537e155f26
Jakub Michalowski <jakub.michalowski@mirumee.com>
2019-01-07 13:55:48 +0100

    Code reformat.

--- tweet_recommendations/testing_framework.py
+++ tweet_recommendations/testing_framework.py
@@ -3,7 +3,10 @@ import random
 import pandas as pd
 import numpy as np
 
-from tweet_recommendations.utils.metrics import get_mean_average_precision_at_k, get_rank_dcg_at_k
+from tweet_recommendations.utils.metrics import (
+    get_mean_average_precision_at_k,
+    get_rank_dcg_at_k,
+)
 
 random.seed(1337)
 config = {"rank_length": 3}
@@ -19,14 +22,19 @@ def get_k_random_hashtags_from_hashtag_data(hashtag_data: pd.DataFrame, k=10):
     return hashtag_list
 
 
-def get_hashtag_rank_for_given_tweets(tweet: str, hashtag_data: pd.DataFrame, config: dict):
-    return get_k_random_hashtags_from_hashtag_data(
-        hashtag_data, config["rank_length"]
-    )
+def get_hashtag_rank_for_given_tweets(
+    tweet: str, hashtag_data: pd.DataFrame, config: dict
+):
+    return get_k_random_hashtags_from_hashtag_data(hashtag_data, config["rank_length"])
 
 
 def get_map_value_for_tweet(test_set: pd.DataFrame, config: dict, hashtag_data=None):
-    test_mAP, test_ndcg = zip(*test_set.apply(lambda row: get_metrices_for_given_dataset(config, hashtag_data, row), axis=1).tolist())
+    test_mAP, test_ndcg = zip(
+        *test_set.apply(
+            lambda row: get_metrices_for_given_dataset(config, hashtag_data, row),
+            axis=1,
+        ).tolist()
+    )
     return np.mean(np.asarray(test_mAP)), np.mean(np.asarray(test_ndcg))
 
 
@@ -35,4 +43,11 @@ def get_metrices_for_given_dataset(config, hashtag_data, row):
         row["tweet"], hashtag_data, config
     )
     expected_hashtags = row["expected_hashtags"]
-    return get_mean_average_precision_at_k(expected_hashtags, predicted_hashtag_rank, config["rank_length"]), get_rank_dcg_at_k(expected_hashtags, predicted_hashtag_rank, config["rank_length"])
+    return (
+        get_mean_average_precision_at_k(
+            expected_hashtags, predicted_hashtag_rank, config["rank_length"]
+        ),
+        get_rank_dcg_at_k(
+            expected_hashtags, predicted_hashtag_rank, config["rank_length"]
+        ),
+    )
