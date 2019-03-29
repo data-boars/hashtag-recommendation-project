c18639870d3b956a8895aa152c52725837ff8391
Kacper Kania <kacper1095@gmail.com>
2019-03-29 21:11:22 +0100

    Added new example, and changed stop criterion value

--- tweet_recommendations/validators/graph_summarization_validator.py
+++ tweet_recommendations/validators/graph_summarization_validator.py
@@ -4,7 +4,7 @@ from tweet_recommendations.other_methods.graph_summarization_for_hashtag_recomme
 from tweet_recommendations.other_methods.lemmatizer import TaggerToygerLemmatizer
 
 lemmatizer = TaggerToygerLemmatizer("data/processed/lemmas.p")
-the_graph = GraphSummarizationMethod(max_iterations=100, minimal_random_walk_change_difference_value=1e-1,
+the_graph = GraphSummarizationMethod(max_iterations=100, minimal_random_walk_change_difference_value=1e-4,
                                      verbose=True, damping_factor=0.8)
 
 with open("data/source_data/original_tweets.p", "rb") as f:
@@ -13,3 +13,4 @@ with open("data/source_data/original_tweets.p", "rb") as f:
 tweets_with_lemmas = lemmatizer.fit_transform(original_tweets, verbose=True)
 the_graph.fit(tweets_with_lemmas, minimal_hashtag_occurence=3)
 print(the_graph.transform("Wiozę szwagra na wybory")[:10])
+print(the_graph.transform("Co by tu dzisiaj zrobić ...")[:10])
