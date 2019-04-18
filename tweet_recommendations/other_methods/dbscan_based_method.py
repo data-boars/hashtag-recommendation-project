f9336efb23d137d16b2e52af825fba7857066c11
Kacper Kania <kacper1095@gmail.com>
2019-04-18 11:03:29 +0200

    Added usage of all available cpu threads so a calculation of DBScan and KDTree is fast as possible (both together approx. 5 mins). Changed representation of medoids and hashtags so during transform there is no need to iterate over them and just perform simple matrix multiplication

--- tweet_recommendations/other_methods/dbscan_based_method.py
+++ tweet_recommendations/other_methods/dbscan_based_method.py
@@ -1,16 +1,17 @@
-from collections import namedtuple, Counter
+import multiprocessing as mp
+from collections import Counter
 from itertools import chain
-from typing import Optional, List, Union
+from typing import List, Optional, Union
 
 import numpy as np
 import pandas as pd
-from scipy.spatial.distance import cosine
+import tqdm
+from scipy.spatial import cKDTree as KDTree, distance
 from sklearn.cluster import DBSCAN
-from sklearn.neighbors.kd_tree import KDTree
+from sklearn.metrics.pairwise import cosine_distances
 
 from tweet_recommendations.other_methods.method import Method
 
-CentroidData = namedtuple("CentroidData", ["cluster_medoid", "popular_hashtags"])
 # "Finally, we use one as the number of minPoints and the average value of this vector as epsilon."
 # according to publication, this value should be fixed to 1.
 MIN_SAMPLES = 1
@@ -23,7 +24,8 @@ class DBScanBasedEstimator(Method):
         self.clusterizer = None
         self.embedding_method = embedding_method
         self.neighbours = None
-        self.centroids_data = {}
+        self._centroids_data = []
+        self._corresponding_to_centroids_data_hashtags = []
 
         self.verbose = verbose
 
@@ -42,7 +44,10 @@ class DBScanBasedEstimator(Method):
         :return: self.
         """
         minimal_hashtag_occurence = fit_params.get("minimal_hashtag_occurence", 10)
-        neighbours_count = fit_params.get("neighbours_count", 2)
+
+        # the value below is so high because otherwise the `epsilon` basing on this would be so small that there would
+        # as many clusters as there are samples in dataset
+        neighbours_count = fit_params.get("neighbours_count", 256)
 
         if self.verbose:
             print(f"Data input shape: {x.shape}")
@@ -65,16 +70,15 @@ class DBScanBasedEstimator(Method):
             print(f"Data embedding shape, after "
                   f"droping data by given criteria: {embeddings.shape}")
 
-        self.neighbours = KDTree(embeddings, metric="euclidean")
-        nearest_neighbors_distances = \
-            self.neighbours.query(embeddings, k=neighbours_count)[0]
-        epsilon = np.mean(nearest_neighbors_distances[:, 1:])
+        self.neighbours = KDTree(embeddings)
+
+        epsilon = np.mean(self.neighbours.query(embeddings, k=neighbours_count, n_jobs=mp.cpu_count())[0])
 
         if self.verbose:
             print(f"Found epsilon: {epsilon}")
+            print("Fitting DBSCAN ... ")
 
-        self.clusterizer = DBSCAN(metric='manhattan', eps=epsilon,
-                                  min_samples=MIN_SAMPLES)
+        self.clusterizer = DBSCAN(metric='manhattan', eps=epsilon, min_samples=MIN_SAMPLES, n_jobs=mp.cpu_count())
         x["cluster_label"] = self.clusterizer.fit_predict(embeddings)
 
         if self.verbose:
@@ -85,51 +89,61 @@ class DBScanBasedEstimator(Method):
         grouped_clusters = x.groupby(["cluster_label"])
 
         if self.verbose:
-            print("Clusters grouped. Building cluster data")
+            print(f"Samples grouped into {len(np.unique(self.clusterizer.labels_))} clusters")
+            print("Building clusters' data")
 
-        for cluster_number, cluster in grouped_clusters:
-            cluster_embeddings = np.vstack(cluster['embedding'].to_numpy())
+        self._centroids_data = []
+        self._corresponding_to_centroids_data_hashtags = []
+
+        for cluster_number, cluster in tqdm.tqdm(grouped_clusters, total=len(np.unique(x["cluster_label"])),
+                                                 disable=not self.verbose):
+            cluster_embeddings = np.vstack(cluster["embedding"].to_numpy())
             cluster_center = np.mean(cluster_embeddings, axis=0).reshape(1, -1)
-            kd_tree = KDTree(cluster_embeddings, metric="euclidean")
-            cluster_medoid_index = kd_tree.query(cluster_center, k=1)[1][0]
+            distances = distance.cdist(cluster_center, cluster_embeddings)[0]  # matrix of shape (1 x N) is returned
+
+            cluster_medoid_index = np.argmin(
+                distances)  # we are sure that it will not point to itself because mean center should be not existent
+
             cluster_medoid = cluster_embeddings[cluster_medoid_index]
             aggregated_hashtags = list(
                 chain.from_iterable(cluster["hashtags"].to_numpy()))
             sorted_most_popular_hashtags = [hashtag for hashtag, count in
                                             Counter(aggregated_hashtags).most_common()]
-            self.centroids_data[cluster_number] = CentroidData(
-                cluster_medoid=cluster_medoid,
-                popular_hashtags=sorted_most_popular_hashtags)
+
+            self._centroids_data.append(cluster_medoid)
+            self._corresponding_to_centroids_data_hashtags.append(sorted_most_popular_hashtags)
+
+        self._centroids_data = np.vstack(self._centroids_data)
+        self._corresponding_to_centroids_data_hashtags = np.asarray(self._corresponding_to_centroids_data_hashtags)
 
         return self
 
-    def transform(self, x: Union[List[str], str]) -> List[List[str]]:
+    def transform(self, x: Union[List[List[str]], List[str]]) -> np.ndarray:
         """
         For a given tweet/tweets embeddings recommend hashtags.
-        :param x: ndarray of tweets embeddings.
+        :param x: list of list of str or list of str. If first argument of x is a list is str, it is assumed that list
+            contains already lemmatized text. If single str is present as first element, it is assumed
+            that lemmatization has to be performed.
         :return: Iterable of recommended hashtags.
         """
-        recommended_hashtags = []
         embeddings = self.embedding_method.transform(x)
-        embeddings = embeddings if len(embeddings.shape) == 2 else embeddings.reshape(1,
-                                                                                      -1)
-        tweets_number = embeddings.shape[0]
-        for i in range(tweets_number):
-            min_dist = np.inf
-            best_hashtags = []
-            for centroid_id, centroid_data in self.centroids_data.items():
-                current_distance = cosine(centroid_data.cluster_medoid,
-                                          embeddings[i, :])
-                if min_dist > current_distance:
-                    min_dist = current_distance
-                    best_hashtags = centroid_data.popular_hashtags
-            recommended_hashtags.append(best_hashtags)
-        return recommended_hashtags
+        embeddings = embeddings if len(embeddings.shape) == 2 else embeddings.reshape(1, -1)
+        distances = cosine_distances(embeddings, self._centroids_data)
+        sorted_distances_indices = np.argsort(distances, axis=1)
+        recommended_hashtags = self._corresponding_to_centroids_data_hashtags[sorted_distances_indices]
+        result = self.post_process_result(recommended_hashtags)
+
+        return result
+
+    @classmethod
+    def post_process_result(cls, recommended_hashtags: np.ndarray) -> np.ndarray:
+        result = []
+        for tweet_tags in recommended_hashtags:
+            result.append([tag for centroid_tags in tweet_tags for tag in centroid_tags])
+        return np.asarray(result)
 
 
-def drop_tweets_with_hashtags_that_occurred_less_than(data: pd.DataFrame,
-                                                      minimal_hashtag_occurrence: int) -> \
-        List[str]:
+def drop_tweets_with_hashtags_that_occurred_less_than(data: pd.DataFrame, minimal_hashtag_occurrence: int) -> List[str]:
     hashtags = data["hashtags"].tolist()
     hashtags = [h['text'] for a_list in hashtags for h in a_list]
     counts = Counter(hashtags)
@@ -138,9 +152,7 @@ def drop_tweets_with_hashtags_that_occurred_less_than(data: pd.DataFrame,
     return filtered_tags
 
 
-def drop_tweets_which_not_contain_given_hashtags(data: pd.DataFrame,
-                                                 filtered_tags: List[
-                                                     str]) -> pd.DataFrame:
+def drop_tweets_which_not_contain_given_hashtags(data: pd.DataFrame, filtered_tags: List[str]) -> pd.DataFrame:
     data["hashtags"] = data["hashtags"].apply(
         lambda x: [elem for elem in x if elem["text"] in filtered_tags])
     data = data.drop(data[data["hashtags"].str.len() == 0].index)
