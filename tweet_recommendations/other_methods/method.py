1f8538f24e6a6874c57accf679f90f6bc91e48e2
Kacper Kania <kacper1095@gmail.com>
2019-04-20 13:30:49 +0200

    Updated method signatures so it uses more general and current way of input data

--- tweet_recommendations/other_methods/method.py
+++ tweet_recommendations/other_methods/method.py
@@ -1,5 +1,7 @@
-from typing import *
+from collections import Counter
+from typing import Optional, Tuple, Union
 
+import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -8,22 +10,18 @@ class Method(TransformerMixin, BaseEstimator):
     def fit(self, x: pd.DataFrame, y: Optional[pd.DataFrame] = None, **fit_params) -> "Method":
         raise NotImplementedError
 
-    def transform(self, x: List[str], **transform_params) -> List[str]:
+    def transform(self, x: Union[Tuple[Tuple[str, ...]], Tuple[str, ...]], **transform_params) -> np.ndarray:
         raise NotImplementedError
 
     @classmethod
     def drop_tweets_with_hashtags_that_occurred_less_than(cls, data: pd.DataFrame,
-                                                          minimal_hashtag_occurrence: int) -> List[str]:
+                                                          minimal_hashtag_occurrence: int) -> pd.DataFrame:
         hashtags = data["hashtags"].tolist()
         hashtags = [h['text'] for a_list in hashtags for h in a_list]
         counts = Counter(hashtags)
         filtered_tags = [t for t, count in counts.items() if
                          count >= minimal_hashtag_occurrence]
-        return filtered_tags
-
-    @classmethod
-    def drop_tweets_without_given_hashtags(cls, data: pd.DataFrame, filtered_tags: List[str]) -> pd.DataFrame:
         data["hashtags"] = data["hashtags"].apply(
             lambda x: [elem for elem in x if elem["text"] in filtered_tags])
-        data = data.drop(data[data["hashtags"].str.len() == 0].index)
+        data = data.drop(data[data["hashtags"].str.len() == 0].index).reset_index()
         return data
