13f1b3a0349878b3d3507c8b77e885a06e5f90df
Michał Kosturek <kosturek.michal@gmail.com>
2019-05-06 22:36:56 +0200

    Added code to prepare training and testing datasets

--- tweet_recommendations/other_methods/method.py
+++ tweet_recommendations/other_methods/method.py
@@ -19,9 +19,10 @@ class Method(TransformerMixin, BaseEstimator):
         hashtags = data["hashtags"].tolist()
         hashtags = [h['text'] for a_list in hashtags for h in a_list]
         counts = Counter(hashtags)
-        filtered_tags = [t for t, count in counts.items() if
-                         count >= minimal_hashtag_occurrence]
+        filtered_tags = {t for t, count in counts.items() if
+                         count >= minimal_hashtag_occurrence}
         data["hashtags"] = data["hashtags"].apply(
             lambda x: [elem for elem in x if elem["text"] in filtered_tags])
+
         data = data.drop(data[data["hashtags"].str.len() == 0].index).reset_index()
         return data
