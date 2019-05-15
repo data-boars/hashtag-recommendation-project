4b0d9e73100677ad5f6878df5be302d637b057ed
kacper1095 <kacper1095@gmail.com>
2019-05-15 10:31:27 +0200

    Implemented more memory efficient one hot encoding (#18)

--- tweet_recommendations/other_methods/utils.py
+++ tweet_recommendations/other_methods/utils.py
@@ -8,14 +8,15 @@ class ModifiedOneHotEncoder(LabelEncoder):
     combining `LabelEncoder` with `OneHotEncoder` without success, so this class was created.
     """
     def __init__(self):
-        self._coding_book: np.ndarray = None
+        self._max_class_num = -1
 
     def fit(self, y):
-        encoding = np.asarray(super().fit_transform(y))
-        self._coding_book = np.eye(np.max(encoding) + 1)
+        encoded = super().fit_transform(y)
+        self._max_class_num = np.max(encoded)
         return self
 
     def transform(self, y):
         encoded = np.asarray(super().transform(y))
-        one_hot = self._coding_book[encoded]
-        return one_hot
+        one_hot_matrix = np.zeros((len(encoded), self._max_class_num))
+        one_hot_matrix[:, y] = 1
+        return one_hot_matrix
