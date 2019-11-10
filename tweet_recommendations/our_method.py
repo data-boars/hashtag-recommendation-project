2e213e87d0cf0f13711a77f6eb30b8a4115719b4
Kacper Kania <kacp.kania@gmail.com>
2019-11-10 14:13:01 +0100

    Refactor to match pep8

--- tweet_recommendations/our_method.py
+++ tweet_recommendations/our_method.py
@@ -1,15 +1,15 @@
-from typing import Optional, Sequence, Union, Callable, Collection, Tuple
+import enum
+from typing import Optional, Sequence, Tuple, Union
 
-from tweet_recommendations.other_methods.method import Method
-from tweet_recommendations.utils.clients import get_wcrft2_results_for_text
-from sklearn.metrics.pairwise import cosine_similarity
-
-import pandas as pd
 import networkx as nx
 import numpy as np
+import pandas as pd
 import tqdm
-import enum
 from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import cosine_similarity
+
+from tweet_recommendations.method import Method
+from tweet_recommendations.utils.clients import get_wcrft2_results_for_text
 
 
 class PopularityMeasure(enum.Enum):
@@ -18,14 +18,16 @@ class PopularityMeasure(enum.Enum):
 
 
 class OurMethod(Method):
-
-    def __init__(self, popularity_to_similarity_ratio: float,
-                 popularity_measure: PopularityMeasure = "pagerank",
-                 path_to_keyedvectors_model: Optional[str] = None,
-                 embedding_name: Optional[str] = "embedding",
-                 personalised_pagerank: bool = True,
-                 weighted_pagerank: bool = True,
-                 verbose: bool = False):
+    def __init__(
+        self,
+        popularity_to_similarity_ratio: float,
+        popularity_measure: PopularityMeasure = "pagerank",
+        path_to_keyedvectors_model: Optional[str] = None,
+        embedding_name: Optional[str] = "embedding",
+        personalised_pagerank: bool = True,
+        weighted_pagerank: bool = True,
+        verbose: bool = False,
+    ):
         """
         Our proposed hashtag recommendation method.
 
@@ -57,8 +59,9 @@ class OurMethod(Method):
         :param verbose: Whether method should be verbose.
         """
 
-        assert 0 <= popularity_to_similarity_ratio <= 1,\
-            "Ratio should be bound between 0 and 1"
+        assert (
+            0 <= popularity_to_similarity_ratio <= 1
+        ), "Ratio should be bound between 0 and 1"
         self.ratio = popularity_to_similarity_ratio
         self.popularity_measure = PopularityMeasure(popularity_measure).value
         self.embedding_name = embedding_name
@@ -68,41 +71,47 @@ class OurMethod(Method):
 
         self.embedding_model = None
         if path_to_keyedvectors_model is not None:
-            self.embedding_model = KeyedVectors.load(path_to_keyedvectors_model,
-                                                     mmap="r")
+            self.embedding_model = KeyedVectors.load(
+                path_to_keyedvectors_model, mmap="r"
+            )
         self.verbose = verbose
 
         self._G: nx.Graph = None
         self._hashtags_df: pd.DataFrame = None
 
-    def fit(self, x: pd.DataFrame,
-            y: Optional[pd.DataFrame] = None, **fit_params):
+    def fit(
+        self, x: pd.DataFrame, y: Optional[pd.DataFrame] = None, **fit_params
+    ):
 
         min_hashtag_count = fit_params.get("min_hashtag_count", 10)
 
         if self.embedding_model is None:
-            assert self.embedding_name in x.columns, \
-                "When no embedding model provided, input DataFrame should " \
-                f"contain column named same as `{self.embedding_name}`."
+            assert (
+                self.embedding_name in x.columns
+            ), "When no embedding model provided, input DataFrame should " f"contain column named same as `{self.embedding_name}`."
         else:
-            assert "lemmas" in x.columns, \
-                "When embedding model is provided input DataFrame should " \
-                'contain column "lemmas".'
+            assert (
+                "lemmas" in x.columns
+            ), "When embedding model is provided input DataFrame should " 'contain column "lemmas".'
 
         x = x[x["hashtags"].str.len() > 0]
         if self.verbose:
             print("Removing tweets with too rare hashtags.")
-        x = self.drop_tweets_with_hashtags_that_occurred_less_than(x, min_hashtag_count)
+        x = self.drop_tweets_with_hashtags_that_occurred_less_than(
+            x, min_hashtag_count
+        )
 
         if isinstance(x["hashtags"].iloc[0][0], dict):
-            x["hashtags"] = x["hashtags"].apply(lambda t: [item["text"]
-                                                           for item in t])
+            x["hashtags"] = x["hashtags"].apply(
+                lambda t: [item["text"] for item in t]
+            )
 
         if self.embedding_model is not None:
             x[self.embedding_name] = self.embed_lemmas(x["lemmas"])
         else:
-            assert len(x[self.embedding_name].iloc[0].shape) == 1, \
-                f"`{self.embedding_name}` column should contain single vectors."
+            assert (
+                len(x[self.embedding_name].iloc[0].shape) == 1
+            ), f"`{self.embedding_name}` column should contain single vectors."
 
         if self.verbose:
             print("Dropping tweets without proper words.")
@@ -126,13 +135,16 @@ class OurMethod(Method):
             print("Calculating PageRank.")
         self._calculate_pagerank()
 
-        hashtags = [{"hashtag": node, **self._G.nodes[node]}
-                    for node in self._G.nodes
-                    if self._G.nodes[node]["node_type"] == "hashtag"]
+        hashtags = [
+            {"hashtag": node, **self._G.nodes[node]}
+            for node in self._G.nodes
+            if self._G.nodes[node]["node_type"] == "hashtag"
+        ]
         self._hashtags_df = pd.DataFrame(hashtags)
 
-    def embed_lemmas(self, texts_lemmas: Union[Sequence[Sequence[str]],
-                                               pd.Series]):
+    def embed_lemmas(
+        self, texts_lemmas: Union[Sequence[Sequence[str]], pd.Series]
+    ):
         if not isinstance(texts_lemmas, pd.Series):
             texts_lemmas = pd.Series(texts_lemmas)
 
@@ -147,6 +159,7 @@ class OurMethod(Method):
                 return np.asarray(vectors).mean(axis=0)
             else:
                 return None
+
         if self.verbose:
             print("Embedding lemmas")
             tqdm.tqdm.pandas()
@@ -154,9 +167,11 @@ class OurMethod(Method):
         else:
             return texts_lemmas.apply(_embed_text)
 
-    def transform(self, x: Union[Tuple[Tuple[str, ...]], Tuple[str, ...],
-                                 Tuple[np.ndarray]],
-                  **transform_params):
+    def transform(
+        self,
+        x: Union[Tuple[Tuple[str, ...]], Tuple[str, ...], Tuple[np.ndarray]],
+        **transform_params,
+    ):
 
         x = list(x)
         # tokenize and lemmatize text
@@ -171,7 +186,9 @@ class OurMethod(Method):
         if len(x[0].shape) > 1:
             x = [emb.mean(axis=0) for emb in x]
 
-        hashtag_emb = np.asarray(self._hashtags_df[self.embedding_name].tolist())
+        hashtag_emb = np.asarray(
+            self._hashtags_df[self.embedding_name].tolist()
+        )
 
         if self.verbose:
             print("Calculating similarities")
@@ -181,7 +198,7 @@ class OurMethod(Method):
         pop = self._hashtags_df[self.popularity_measure].to_numpy()
         pop = self.normalise(pop.reshape(1, -1))
 
-        sim_pop = ((1 - self.ratio) * sim + (self.ratio * pop))
+        sim_pop = (1 - self.ratio) * sim + (self.ratio * pop)
         if self.verbose:
             print("Calculating ranking.")
         rankings = np.argsort(-sim_pop, axis=1)
@@ -231,18 +248,20 @@ class OurMethod(Method):
 
         if self.verbose:
             tqdm.tqdm.pandas()
-            tweets.progress_apply(lambda r:
-                                  add_row_to_graph(r, G, self.embedding_name),
-                                  axis=1)
+            tweets.progress_apply(
+                lambda r: add_row_to_graph(r, G, self.embedding_name), axis=1
+            )
         else:
-            tweets.apply(lambda r: add_row_to_graph(r, G, self.embedding_name),
-                         axis=1)
+            tweets.apply(
+                lambda r: add_row_to_graph(r, G, self.embedding_name), axis=1
+            )
         return G
 
     def _calculate_hashtag_embeddings_and_mean_retweets(self):
 
-        for node in tqdm.tqdm(self._G.nodes, total=len(self._G.nodes),
-                              disable=not self.verbose):
+        for node in tqdm.tqdm(
+            self._G.nodes, total=len(self._G.nodes), disable=not self.verbose
+        ):
             if self._G.nodes[node]["node_type"] == "hashtag":
                 embeddings, retweets_counts = [], []
                 for neighbour in self._G.neighbors(node):
@@ -270,21 +289,23 @@ class OurMethod(Method):
         else:
             pers_vector = None
 
-        graph_pagerank = nx.pagerank(self._G,
-                                     weight=edge_weight_key,
-                                     personalization=pers_vector)
+        graph_pagerank = nx.pagerank(
+            self._G, weight=edge_weight_key, personalization=pers_vector
+        )
         nx.set_node_attributes(self._G, graph_pagerank, "pagerank")
 
     def _calculate_edge_weights(self):
-        for (node_from, node_to,
-             edge_features) in tqdm.tqdm(self._G.edges(data=True),
-                                         total=len(self._G.edges),
-                                         disable=not self.verbose):
+        for (node_from, node_to, edge_features) in tqdm.tqdm(
+            self._G.edges(data=True),
+            total=len(self._G.edges),
+            disable=not self.verbose,
+        ):
             emb_from = self._G.nodes[node_from][self.embedding_name]
             emb_to = self._G.nodes[node_to][self.embedding_name]
 
-            sim = self.embedding_similarity(emb_from.reshape(1, -1),
-                                            emb_to.reshape(1, -1)).item()
+            sim = self.embedding_similarity(
+                emb_from.reshape(1, -1), emb_to.reshape(1, -1)
+            ).item()
 
             edge_features["distance"] = 1 - sim
             edge_features["similarity"] = sim
