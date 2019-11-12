6d6e2f6666fddea9c5f4e3c5fe5708ad6ec4f799
Kacper Kania <kacp.kania@gmail.com>
2019-11-12 10:36:09 +0100

    Change paths

--- tweet_recommendations/scripts/our_runner.sh
+++ tweet_recommendations/scripts/our_runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./predictions_dbscan_based_method_our_dataset.sh \
-    && ./predictions_graph_based_method_our_dataset.sh \
-    && ./predictions_our_method_our_dataset.sh \
-    && ./predictions_tweet2vec_method_our_dataset.sh
+./tweet_recommendations/scripts/predictions_dbscan_based_method_our_dataset.sh \
+    && ./tweet_recommendations/scripts/predictions_graph_based_method_our_dataset.sh \
+    && ./tweet_recommendations/scripts/predictions_our_method_our_dataset.sh \
+    && ./tweet_recommendations/scripts/predictions_tweet2vec_method_our_dataset.sh
--- tweet_recommendations/scripts/udi_runner.sh
+++ tweet_recommendations/scripts/udi_runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./predictions_dbscan_based_method_udi_dataset.sh \
-    && ./predictions_graph_based_method_udi_dataset.sh \
-    && ./predictions_our_method_udi_dataset.sh \
-    && ./predictions_tweet2vec_method_udi_dataset.sh
+./tweet_recommendations/scripts/predictions_dbscan_based_method_udi_dataset.sh \
+    && ./tweet_recommendations/scripts/predictions_graph_based_method_udi_dataset.sh \
+    && ./tweet_recommendations/scripts/predictions_our_method_udi_dataset.sh \
+    && ./tweet_recommendations/scripts/predictions_tweet2vec_method_udi_dataset.sh
